07b72bd9759ae70b65cb86faa397064bed116528
Abraham Ragab <Abrahammoustafa@hotmail.co.uk>
2018-12-22 19:20:56 +0200

    Add nyt articles

--- js/script.js
+++ js/script.js
@@ -20,6 +20,28 @@ function loadData() {
     var sreetViewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '&key=AIzaSyDX5gXzgo_JeZMs_pajtIqigPnKVIPBxTE';
     // YOUR CODE GOES HERE!
     $body.append('<img class="bgimg" src="' + sreetViewUrl + '">')
+    /**
+     I didn't get an api key :s 
+     
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+    $.getJSON(nytimesUrl, function(data){
+
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
+        };
+
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
+    */
+
     return false;
 };
 
